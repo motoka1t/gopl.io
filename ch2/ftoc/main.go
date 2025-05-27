5e513177ac4ecf9c5b11fe299e003dce7335fd2b
Toshiki Motokawa <motoka1t@gmail.com>
2025-05-28 08:30:13 +0900

    ch2

---
+++ ch2/ftoc/main.go
@@ -0,0 +1,14 @@
+// ftocは、華氏(Fahrenheit)と摂氏(calsius)への変換を二つ表示します。
+package main
+
+import "fmt"
+
+func main() {
+	const freezingF, boilingF = 32.0, 212.0
+	fmt.Printf("%g'F = %g'C\n", freezingF, fToC(freezingF)) // "32F=0C"
+	fmt.Printf("%g'F = %g'C\n", boilingF, fToC(boilingF))   // "212F=100C"
+}
+
+func fToC(f float64) float64 {
+	return (f - 32) * 5 / 9
+}
